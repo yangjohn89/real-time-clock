06b1cf22a72d950234a61efebfe8c7ab81351db6
Yang Yu <Yang Yu>
2016-06-24 20:02:46 -0700

    This is real time clock

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('currentTime').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 1000)
+}
+function checkTime(i) {
+    if (i < 10) 
+    {
+    i = "0" + i
+    };  
+    return i;
+}
+var currentTime = document.getElementById('currentTime')
+currentTime.onload = startTime()
